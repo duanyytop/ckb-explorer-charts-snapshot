a0b1e3f508124735cd45900319d3be2a1d0e31a2
duanyytop <duanyytop@gmail.com>
2020-03-09 14:58:33 +0800

    feat: add all charts url and path

--- index.js
+++ index.js
@@ -8,6 +8,34 @@ const url_paths = [
   {
     url: "https://explorer.nervos.org/charts/difficulty_uncle_rate",
     path: "path/difficulty_uncle_rate.png"
+  },
+  {
+    url: "https://explorer.nervos.org/charts/difficulty",
+    path: "path/difficulty.png"
+  },
+  {
+    url: "https://explorer.nervos.org/charts/hash_rate",
+    path: "path/hash_rate.png"
+  },
+  {
+    url: "https://explorer.nervos.org/charts/uncle_rate",
+    path: "path/uncle_rate.png"
+  },
+  {
+    url: "https://explorer.nervos.org/charts/transaction_count",
+    path: "path/transaction_count.png"
+  },
+  {
+    url: "https://explorer.nervos.org/charts/address_count",
+    path: "path/address_count.png"
+  },
+  {
+    url: "https://explorer.nervos.org/charts/cell_count",
+    path: "path/cell_count.png"
+  },
+  {
+    url: "https://explorer.nervos.org/charts/total_dao_deposit",
+    path: "path/total_dao_deposit.png"
   }
 ];
 
--- snapshot.js
+++ snapshot.js
@@ -3,12 +3,15 @@ const puppeteer = require("puppeteer");
 exports.snapshot = async function(url, path) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  try {
+    await page.goto(url, {timeout: 120000});
+  } catch (error) {
+    console.error(`Open page error: ${error}`);
+  }
   const dimensions = await page.evaluate(() => {
     return {
       width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio
+      height: document.documentElement.clientHeight
     };
   });
   const clip = {
@@ -17,6 +20,10 @@ exports.snapshot = async function(url, path) {
     width: dimensions.width * 0.8,
     height: dimensions.height * 0.78
   };
-  await page.screenshot({path, clip});
+  try {
+    await page.screenshot({path, clip});
+  } catch (error) {
+    console.error(`Screenshot error: ${error}`);
+  }
   await browser.close();
 };
